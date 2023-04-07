2930af0f86501a25207b258afa89dafa7ffec0fd
Kavya Subramanian <kavyasub@unc.edu>
2023-04-07 14:47:21 -0400

    Changes made to console log messages

--- cli.js
+++ cli.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
-
 import  minimist  from 'minimist'
 import  moment  from 'moment-timezone'
 import  fetch  from 'node-fetch'
-//import exit from 'node:process'
 
 const timezone = moment.tz.guess();
 const args = minimist(process.argv.slice(2));
 
-if (args.h) {
-    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n")
-    console.log("-h            Show this help message and exit.\n")
-    console.log("-n, -s        Latitude: N positive; S negative.\n")
-    console.log("-e, -w        Longitude: E positive; W negative.\n")
-    console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.\n")
-    console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n")
-    console.log("-j            Echo pretty JSON from open-meteo API and exit.\n")
-    process.exit(0)
+if (args.h){
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+        -h            Show this help message and exit.
+        -n, -s        Latitude: N positive; S negative.
+        -e, -w        Longitude: E positive; W negative.
+        -z            Time zone: uses tz.guess() from moment-timezone by default.
+        -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+        -j            Echo pretty JSON from open-meteo API and exit.
+    `);
+    process.exit(0);
 }
 
 let latitude;
@@ -49,20 +48,20 @@ else if(args.n) {
 } 
 
 
-//Days constant
-const days = args.n;
-
 //Construct url
 const url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours";
 const response = await fetch(url)
 const data = await response.json()
 
+//Days constant
+const days = args.n;
+
 //Output message based on data
 let message; 
 if (data.daily.precipitation_hours[days] > 0) {
-    message = "Grab an umbrella!"
+    message = "Grab an umbrella "
 } else {
-    message = "It's a great day! Go outside!"
+    message = "It's a great day "
 }
 
 if (days == 0) {
